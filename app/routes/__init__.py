42d6f0d8c172610d17d577b26b26c5372de26d49
naizgi hayilekiros <163419887+Naizgi@users.noreply.github.com>
2026-05-01 14:26:03 +0300

    edited the db config

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,4 @@
 # app/routers/__init__.py
-# app/routers/__init__.py
 from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
 from app.routers.branches import router as branches_router
@@ -19,21 +18,23 @@ from app.routers.dashboard import router as dashboard_router
 from app.routers.settings import router as settings_router
 from app.routers.temp_items import router as temp_items_router
 
-# Subscription routers are imported directly in main.py
-
-
-# Export all routers
 __all__ = [
     'auth_router',
-    'branches_router', 
-    'products_router',
     'users_router',
+    'branches_router',
+    'tenants_router',
+    'categories_router',
+    'units_router',
+    'products_router',
+    'batches_router',
     'stock_router',
     'sales_router',
-    'purchase_router',
-    'reports_router',
+    'returns_router',
+    'purchases_router',
+    'loans_router',
     'alerts_router',
+    'reports_router',
     'dashboard_router',
-    'loan_router',
+    'settings_router',
     'temp_items_router'
 ]
